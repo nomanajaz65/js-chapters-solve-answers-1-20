e1edd8e1420af15d8deba44a1e1af6c310fb8363
nomanajaz65 <nomanajaz65@gmail.com>
2023-02-08 05:13:27 -0800

    first msg

---
+++ app.js
@@ -0,0 +1,509 @@
+// chapter 1
+
+// ⦁	Write a script to greet your website visitor using JS alert box.
+// ⦁	Write a script to display following message on your web page:
+// ⦁	Write a script to display following message on your web page: (Hint : Use line break)
+
+// alert("Welcome to my Site!");
+
+// alert("Error! Please enter a valid password.");
+
+// alert("Welcome to JS Land... \n Happy Coding!");
+
+
+
+
+                            // chapter 2
+
+// declare a variable called username ?
+// var username;
+
+//Q2:declare a variable called myName & assign it to a string that represents
+        // your full name ?
+        // var myName = "Noman Ajaz";
+
+// //  Q3:write a script to:
+//        a. declare a JS variable, titled MESSAGE
+//        b. assign "hello world" to the variable message
+//        c. display the message in alert
+
+// var message;
+
+//  message = "Hello World!"
+
+//  alert(message);
+
+
+                //  CHAPTER 3
+// Q1
+
+// var age = 22;
+// alert("I am " + 22 + " years old.");
+               
+// Q2
+ // var visits = 1;
+//  alert("You have visited this site " + visits + " times")
+
+// Q3 
+ //      var birthYear= 2001;
+// document.write("My birth year is " + 2001 + "<br>");
+//     document.write("Data type of my decalred variable is " + "NUMBER" + "<br>");
+                
+// Q4
+ // var visitorName = "SHARJEEL";
+// var productTitle = "T-shirt(s)";
+// var quantity = 5;
+// document.write("<b>" + "SHARJEEL" + "</b>" + " Ordered " + " " + "5 T-shirts(s)" + " " + "</b>" + " On XYZ Clothing Store");
+                
+
+
+                  // chapter 4
+
+// var name1, name2, name3;
+
+// var _name,name,Name,name1,$name;
+// //var 1name, %name, function, .name,
+
+// document.write("<h2>Rules for naming JS variables</h2><br>");
+// document.write("Variable names can only contain ,numbres,$ and _ . For example $my_1stVariable<br>");
+// document.write("Variables must begin with a letter,$ or _ . For example $name, _name or name<br>");
+// document.write("Variable names are case sensitive<br>");
+// document.write("Variable names should not be JS Keywords<br>");
+
+                //    chapter 5  TO 7
+
+
+
+                // var num1 = 13;
+                // var num2 = 9;
+                // var sum = num1 + num2;
+                // document.write("Sum of " +num1+ " and " +num2+" is "+ sum +"<br>");
+                
+                //  var num1 = 13;
+                //  var num2 = 9;
+                //  var sum = num1 - num2;
+                //  document.write("<h3>Subtraction of " +num1+ " and " +num2+" is "+ sum +"</h3><br>");
+                
+                //  var num1 = 13;
+                //  var num2 = 9;
+                //  var sum = num1 / num2;
+                //  document.write("<h3>Division of " +num1+ " and " +num2+" is "+ sum +"</h3><br>");
+                
+                //  var num1 = 13;
+                // var num2 = 9;
+                // var sum = num1 * num2;
+                //  document.write("<h3>Multiplication of " +num1+ " and " +num2+" is "+ sum +"</h3><br>");
+                
+                // var num1 = 13;
+                // var num2 = 9;
+                // var sum = num1 % num2;
+                // document.write("<h3>Modulus of " +num1+ " and " +num2+" is "+ sum +"</h3><br>");
+                
+                //    question : 3   PARTS
+
+                //  var num;
+                //  document.write("Value after variable declaration is: "+ 1 +"<br>");
+                // num = 12;
+                // document.write("Initial value: " + 3 + "<br>");
+                //  num +=1;
+                //  document.write("Value after increment is: " + num + "<br>");
+                //  num += 7;
+                //  document.write("Value after Addition is: " + num + "<br>");
+                //  num -=1;
+                //  document.write("Value after decrement is: " + num + "<br>");
+                // num /=3;
+                //  document.write("The remainder is: " + num + "<br>");
+
+                // Q4
+                //  var costOfMovieTicket = 600;
+                //  var costOfFiveTickets = 5* costOfMovieTicket;
+                //  document.write("Total cost to buy 5 tickets to a movie is: " + costOfFiveTickets + "PKR<br><br>");
+                
+                // Q5 
+                //  var number = prompt("Enter the number :");
+                //  var range = prompt("Enter the range :");
+                
+                //  document.write("Table of " + number  + "<br>");
+                //  for (i = 1; i<= range; i++)
+                //  {
+                //      document.write("<b>" + number + " x " + i + " = " + number*i + "</b><br>");
+                //  }
+
+
+                    //    CHAPTER 8  TO 11
+
+
+// question1
+// 1. Write a program that takes input a name from user &
+// greet the user.
+
+// var name=prompt("Whats your name?");
+// alert("Your name is " + name);
+
+
+// question2
+//  Write a program to take input a number from user &
+// display it’s multiplication table on your browser. If user
+// does not enter a new number, multiplication table of 5
+// should be displayed by default.
+
+// var num=prompt("Enter a number: ");
+// var num1=5;
+// var mul;
+// if(!num)
+// {
+//     for(var i=1;i<=10;i++)
+//     {
+//         mul=num1*i;
+//         document.write(num1+" x "+i+" = "+mul+"<br>");
+//     }
+// }
+// else
+// {
+//     for(var i=1;i<=10;i++)
+//     {
+//         mul=num*i;
+//         document.write(num+" x "+i+" = "+mul+"<br>");
+//     }
+
+// question 3
+
+//  Write a program to take “city” name as input from user. If
+// user enters “Karachi”, welcome the user like this:
+// “Welcome to city of lights”
+// var city=prompt("Enter your city name?");
+
+// if(city=="karachi")
+// {
+//     alert("Welcome to city of lights");
+// }
+// else{
+//     alert("We are not ordered to welcome you");
+// }
+
+
+// question4
+//  Write a program to take “gender” as input from user. If the
+// user is male, give the message: Good Morning Sir. If the
+// user is female, give the message: Good Morning Ma’am.
+// var gender=prompt("Enter your gender");
+
+// if(gender=="male")
+// {
+//     alert("Good Morning Sir");
+// }
+// else if(gender=="female"){
+//     alert("Good Morning Ma'm");
+// }
+
+
+// question 5
+
+//  Write a program to take input color of road traffic signal
+// from the user & show the message according to this table:
+// Signal color Message
+// Red Must Stop
+// Yellow Ready to move
+// Green Move now
+// var color=prompt("Input color of road traffic signal");
+
+// if(color==="red")
+// {
+//     alert("Must stop");
+// }
+// else if(color==="yellow")
+// {
+//     alert("ready to move");
+// }
+// else if(color==="green")
+// {
+//     alert("move now");
+// }
+// else
+// {
+//     alert("Enter right value");
+// }
+
+                 //   CHAPTER 12 - 13
+
+
+
+
+
+// question1
+// 1. Write a JavaScript program that accept two integers and
+// display the larger. Also show if the two integers are equal.
+
+//  var num=prompt("Enter the first number");
+// var num1=parseInt(num);
+//  num=prompt("Enter the second number");
+//  var num2=parseInt(num);
+//  if(num1>num2)
+//  {
+//      alert("The number 1 is larger");
+//  }
+//  else if(num2>num1)
+//  {
+//      alert("The number 2 is larger");
+//  }
+//  else if(num1==num2)
+// {
+//      alert("The number 1 and number 2 are equal");
+//  }
+
+
+// // question2
+// //  Write a program that takes input a number from user &
+// // state whether the number is positive, negative or zero.
+//  var num=prompt("Enter the number");
+//  var num1=parseInt(num);
+//  if(num1===0)
+//  {
+//      alert("The number is equal to zero");
+//  }
+//  else if(num1>0)
+//  {
+//     alert("The number is positive");
+//  }
+//  else if(num1<0)
+//  {
+//      alert("The number is negative");
+//  }
+
+
+// // question3
+// //  Write a program that takes a character (i.e. string of
+// // length 1) and returns true if it is a vowel, false otherwise
+
+//  var len=prompt("Enter a character i.e(The length of string 1)");
+//  var str=len.length;       
+//                     //  length (counting)
+//  str=parseInt(str);
+//  if(str===1)
+//  {
+//     len=len.toLowerCase();
+//                         //  tolowercase(describe)
+//      if(len==="a"||len==="e"||len==="i"||len==="o"||len==="u")
+//     {
+//          alert("True its a vowel");
+//      }
+//      else{
+//         alert("False its not a vowel");
+//      }
+//  }
+//  else{
+//      alert("Please Enter a character i.e(The length of string 1)");
+//  }
+
+
+// // question4
+// //    Write a program that
+// // a. Store correct password in a JS variable.
+// // b. Asks user to enter his/her password
+// // c. Validate the two passwords:
+// //           i. Check if user has entered password. If not, then
+// //              give message “ Please enter your password”
+// //           ii. Check if both passwords are same. If they are
+// //               same, show message “Correct! The password you
+// //               entered matches the original password”. Show
+// //              “Incorrect password” otherwise.
+
+
+//  var pas1=prompt("Please! Enter a password");
+//  var len=pas1.length;
+//  len=parseInt(len);
+//  while(len===0)
+//  {
+//      alert("Please enter a password here");
+//      pas1=prompt("Please! Enter a password");
+//      len=pas1.length;
+//      len=parseInt(len);
+//      if(len!=0)
+//      {
+//          break;
+//      }
+//  }
+//  var pas2=prompt("Please!Re-Enter the password");
+//  if(pas1===pas2)
+//  {
+//      alert("Correct the password entered matches the original password");
+// }
+//  else if(pas1!=pas2)
+//  {
+//     alert("Incorrect password");
+//  }
+
+        
+
+// // question5
+// // Q: This if/else statement does not work. Try to fix it:
+
+// var greeting;
+//  var hour = 13;
+//  if (hour < 18) {
+//   greeting = "Good day";
+//  alert(greeting);
+//  }
+//  else
+//  {
+//  greeting = "Good evening";
+//  alert(greeting);
+// }
+//  The brackets {} were not there correctly
+
+
+                //   chapter 14 - 16
+
+// / Chapter 14-17   ARRAYS 
+
+// // question1
+// // 1. Declare an empty array using JS literal notation to store
+// // student names in future.
+// var arrStudent=[];
+
+
+// // question2
+// // 2. Declare an empty array using JS object notation to store
+// // student names in future.
+// var studentArray=new Array();
+
+
+// // question3
+// // 3. Declare and initialize a strings array
+// var arrString=["bilal","ammar","hassan","ali"];
+
+
+// // question4
+// // 4. Declare and initialize a numbers array.
+// var arrNum=[1,2,3,4,5];
+
+
+
+// // question5
+// // 5. Declare and initialize a boolean array.
+// var arrBoolean=[1,0,false,true];
+// alert(arrBoolean);
+
+
+            //    CHAPTER 17 - 20
+
+            // question1
+// 1. Write a program to print numeric counting from 1 to 10.
+// for(var i=0;i<10;i++)
+// {
+//     document.write((i+1)+"<br>");
+
+
+
+                  // question2
+
+                  // 2. Write a program to print multiplication table of any
+// number using for loop. Table number & length should be
+// taken as an input from user.
+
+// var number = prompt("Enter a number to show its multiplication table");
+// number=parseInt(number);
+// var mul;
+// var length=prompt("Enter the length of multiplication table");
+// length=parseInt(length);
+// while(length===-1||length===0)
+// {
+//     alert("Please! Enter a positive number for length");
+//     length=prompt("Enter the length of multiplication table");
+//     length=parseInt(length);
+//     if(length>0)
+//     {
+//         break;
+//         alert("Correct Now see the table");
+//     }
+// }
+// document.write("Multiplication table of &nbsp;"+number+"<br>");
+// document.write("Length &nbsp;"+length+"<br>");
+// for(var i=1;i<=length;i++)
+// {
+//     mul=number*i;
+//     document.write("<br>"+number+" x "+i+" = "+mul);
+// }
+
+
+
+// question3
+// 3. Write a program to print items of the following array
+// using for loop:
+// fruits = [“apple”, “banana”, “mango”, “orange”,“strawberry”]
+// var fruits = ["apple","banana","mango","orange","strawberry"];
+// var size=fruits.length;
+// for(var i=0;i<size;i++)
+// {
+//     document.write(fruits[i]+"<br>");
+// }
+// for(var i=0;i<size;i++)
+// {
+//     document.write("Element at index "+i+" is "+fruits[i]+"<br>");
+// }
+
+
+
+                         // question4
+
+// 4. Generate the following series in your browser. See
+// example output.
+// a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+// document.write("<b>Counting:</b><br>");
+// for(var i=1;i<=15;i++)
+// {
+//     document.write(i+" , ");
+// }
+// // b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+// document.write("<br><br><b>Reverse Counting:</b><br>");
+// for(var i=15;i>=1;i--)
+// {
+//     document.write(i+" , ");
+// }
+// // c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+// document.write("<br><br><b>Even:</b><br>");
+// for(var i=0;i<=20;i=i+2)
+// {
+//     document.write(i+" , ");
+// }
+// // d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+// document.write("<br><br><b>Odd:</b><br>");
+// for(var i=1;i<=20;i=i+2)
+// {
+//     document.write(i+" , ");
+// }
+// // e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
+// document.write("<br><br><b>Series:</b><br>");
+// for(var i=0;i<=20;i=i+2)
+// {
+//     document.write(i+"k, ");
+// }
+
+
+
+                 // question5
+
+ // 5. You have an array
+// A = [“cake”, “apple pie”, “cookie”, “chips”, “patties”]
+// Write a program to enable “search by user input” in an
+// array.
+// After searching, prompt the user whether the given item is
+// found in the list or not. Example:
+
+// var A = ["cake","apple pie","cookie","chips","patties"];
+// var search=prompt("Welcome to ABC Bakery.What do you want to order Sir/Ma'm");
+// var size=A.length;
+// var check=false;
+// for(var i=0;i<=size;i++)
+// {
+//     if(search===A[i])
+//     {
+//         alert(search+" is available at index "+(i+1)+" of our bakery");
+//         check=true;
+//         break;
+//     }
+// }
+// if(check==false)
+// {
+//     alert("We are sorry "+search  +" is not available in our bakery");
+// }
